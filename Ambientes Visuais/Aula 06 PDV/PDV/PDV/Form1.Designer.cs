5020cb87faf0aefe11f62326200b64ba6ea4eb6f
Jóta <franciscojunior02@hotmail.com.br>
2017-03-23 09:05:13 -0300

    PDV
    form, classe produtos e lista

---
+++ Ambientes Visuais/Aula 06 PDV/PDV/PDV/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PDV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbProduto = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.btIncluir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbProduto);
+            this.panel1.Location = new System.Drawing.Point(30, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(825, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbProduto
+            // 
+            this.lbProduto.AutoSize = true;
+            this.lbProduto.Location = new System.Drawing.Point(276, 20);
+            this.lbProduto.Name = "lbProduto";
+            this.lbProduto.Size = new System.Drawing.Size(110, 13);
+            this.lbProduto.TabIndex = 0;
+            this.lbProduto.Text = "Descrição do Produto";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btIncluir);
+            this.groupBox1.Controls.Add(this.txtPreco);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.txtQtd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 182);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Produto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantidade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Preço Unitário";
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(10, 77);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(100, 20);
+            this.txtQtd.TabIndex = 3;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(10, 38);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(10, 116);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(100, 20);
+            this.txtPreco.TabIndex = 5;
+            // 
+            // btIncluir
+            // 
+            this.btIncluir.Location = new System.Drawing.Point(10, 142);
+            this.btIncluir.Name = "btIncluir";
+            this.btIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btIncluir.TabIndex = 2;
+            this.btIncluir.Text = "Incluir";
+            this.btIncluir.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Remover";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 311);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbProduto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btIncluir;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ambientes Visuais/Aula 06 PDV/PDV/PDV/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV
+{
+    class Produto
+    {
+        //propriedades
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double PrecoUnitario { get; set; }
+        public int Qtd { get; set; }
+
+        //contrutores
+        public Produto()
+        {
+            //vazio
+        }
+
+        public Produto(int codigo, string descricao, double precoUnitario, int qtd)
+        {
+            this.Codigo = codigo;
+            this.Descricao = descricao;
+            this.PrecoUnitario = precoUnitario;
+            this.Qtd = qtd;
+        }
+
+        private List<Produto> getProdutos()
+        {
+            List<Produto> listaProdutos = new List<Produto>();
+            listaProdutos.Add(new Produto(1, "Mamão", 7.50,1));
+            listaProdutos.Add(new Produto(2, "Pera", 10.20,1));
+            listaProdutos.Add(new Produto(3,"Batata", 4.35,1));
+            listaProdutos.Add(new Produto(4,"Cenoura", 6.10,1));
+            listaProdutos.Add(new Produto(5,"Limão", 2.50,1));
+            listaProdutos.Add(new Produto(6,"Alho", 4.30,1));
+            listaProdutos.Add(new Produto(7,"Alface",3.10,1));
+            listaProdutos.Add(new Produto(8,"Maça", 7.40,1));
+            listaProdutos.Add(new Produto(9,"Acelga",6.10,1));
+            listaProdutos.Add(new Produto(10,"Maracujá",8.60,1));
+
+            return listaProdutos;
+        }
+
+        public Produto bucaPorCodigo(int codigo)
+        {
+            Produto produtoBusca = null;
+            foreach (var produto in getProdutos())
+            {
+                if (produto.Codigo == codigo)
+                {
+                    produtoBusca = produto;
+                    break;
+                }
+            }
+
+            return produtoBusca;
+        }
+
+    }
+}
