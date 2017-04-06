3ec83f55951c9a751d5c4864db536456a040f541
Jóta <franciscojunior02@hotmail.com.br>
2017-04-06 09:22:10 -0300

    classe veiculo com lista

--- Ambientes Visuais/Prova/Prova/Veiculos.cs
+++ Ambientes Visuais/Prova/Prova/Veiculos.cs
@@ -8,10 +8,49 @@ namespace Prova
 {
     class Veiculos
     {
-        public int condigo { get; set; }
+        public int cod { get; set; }
         public string marca { get; set; }
         public string modelo { get; set; }
         public double valorAluguel { get; set; }
+
+        //contrutores
+        public Veiculos()
+        {
+
+        }
+
+        //contruttor com parametro
+        public Veiculos(int Cod, string Marca, string Modelo, double ValorAluguel)
+        {
+            this.cod = cod;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.valorAluguel = valorAluguel;
+        }
+
+        private List<Veiculos> getVeiculos()
+        {
+            List<Veiculos> lista = new List<Veiculos>();
+            lista.Add(new Veiculos(1,"Ford", "Ka", 70.0));
+            lista.Add(new Veiculos(2, "Fiat", "Uno", 70.0));
+            lista.Add(new Veiculos(3, "Volkswagem","Gol",70.0));
+
+            return lista;
+        }
+
+        public Veiculos BuscaCod(int cod)
+        {
+            Veiculos VeiculoBusca = null;
+            foreach (var veiculo in getVeiculos())
+            {
+                if (veiculo.cod == cod)
+                {
+                    VeiculoBusca = veiculo;
+                    break;
+                }
+            }
+            return VeiculoBusca;
+        }
 
     }
 }
